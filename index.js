141912f7093f400b1c8c9f5ccd15de296116463b
Amanda Smith <ajs.amandasmith@gmail.com>
2022-03-16 23:36:40 -0500

    commit tree 1053e17ab69dfe78605bbf38a75b85ecddfa3d86

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+// write a function called 'submitData'
+  // take two strings as arguments
+    // one representing a user's name
+    // the other representing a user's email
+
+    function submitData(userName, userEMail) {
+      return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEMail
+        })
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const body = document.querySelector('body');
+        body.innerHTML = object.id;
+      })
+      .catch(function (error) {
+        const body = document.querySelector('body');
+        body.innerHTML = error.message;
+      })
+    }
+
+    submitData();
